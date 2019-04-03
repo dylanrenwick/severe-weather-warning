92aa6188df60bbefd068ad1686ce5aa00d83b1d1
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-02 22:03:54 -0400

    Added isLoaded flag to prevent Init running every time main menu loads

--- RoR2ML/Loader.cs
+++ RoR2ML/Loader.cs
@@ -7,14 +7,18 @@ namespace RoR2ML
     {
         private const string ML_VER = "v0.1.0";
         
+        private static bool isLoaded = false;
+        
         public static void Init()
         {
+            if (isLoaded) return;
             // This is our "entry point" where stuff starts
             
             // We need to create a GameObject running a custom MonoBehaviour
             // Then flag it as DontDestroyOnLoad so that it remains persistent
             
             Log("RoR2ML Installed");
+            isLoaded = true;
         }
 
         public static void Log(string message)
